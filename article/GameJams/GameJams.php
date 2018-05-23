c97c15fdb9f62a853b927868fe1a77f1cd03b115
Loris Giraud <lorisg01@yahoo.fr>
2018-05-23 10:17:50 +0300

    Game Jams Article added

---
+++ article/GameJams/GameJams.php
@@ -0,0 +1,76 @@
+<html>
+    <head>
+        <meta charset=utf-8>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> LOUD 2018 </title>
+        <link style="text/css" rel="stylesheet" href="article.css">
+        <link style="text/css" rel="stylesheet" href="../commun.css">
+        <link href="oswald" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville|Oswald:300|Source+Sans+Pro:200,300,400" rel="stylesheet">
+    </head>
+    <body>
+        <?php include("../top.php"); ?>
+
+        <div class="white_square">
+          <div class="text">
+            <p id="header">A DIG INTO GAME DEVELOPMENT</p>
+
+            <p id="first">Nowadays, a lot of video games are created by big companies. Though, there is an underground network of independant creators, who unite themselves into creating plenty of original little gems. Let's have a look at them, shall we ?</p>
+
+            <p id="author">Text: Loris GIRAUD. Art: Open Pixel Project
+            </p>
+
+            <img id="image1" src="beastlands.jpg" alt="Moomin">
+            <figcaption>A classic game Pixel Art</figcaption>
+			  
+			<p id="main">THE COVER OF THE MINE
+            </p>
+			
+            <p>
+            In 2017, 364 million people assisted to the Worlds Championship of League of Legends at Pekin, in China. The same year, Pokemon Go blew up everyone with peaks at 25 million daily players. During the 100 first days of Fortnite, half a trillion shots were fired with now up to 20 million of players from all around the globe. Twitch, a live streaming website, mainly based on video games, has more viewers than HBO and Netflix combined. In January 2018, the website had even beat CNN.
+			The e-sport scene has recently burst in, with an European market estimated at over 300 million of American dollars. Video games are gaining more and more users and spectators, with new games produced each month. The Nintendo announcement for the fifth game in the Super Smash Brothers Series Behind this gigantic window of well-known games, lies plenty of original, sometimes strange gems known as “indie games”.
+            </p>
+
+            <p id="main">THE INDEPENDENT VEIN
+            </p>
+
+            <p>The word “indie” is wide though, and not only limited to games. In fact, it describes anything that is being produced underground, outside of the traditional commercial way. Therefore, every game created without the help of any publisher can be considered as one. Even though, there are still games in the middle, that could be called “indie” while being supported by a publisher. Mainly because they have the spirit of them, as players find they were created from passionate creators. It’s the case of Ori and the Blind Forest. A gorgeous 2D game created by Moon Studios, a small team which is nevertheless supported by Microsoft. 
+            </p>
+
+              <figure>
+                <img id="image2" src="spr_m_traveler_walk_anim.gif" alt="A pixelated animation">
+                <figcaption id="figcaptionside">A pixelated animation</figcaption>
+              </figure>
+
+            <p>These games produced in independent studios with often way less budget than AAA games, are rather hidden between them. Some of them reach success like Minecraft, Braid, World of Goo, Undertale and many more. A lot more are created by gamers to be played by fans. This is the case of The Stanley Parable, Limbo or Spelunky. The popularity and number of them has increased in the latter half of 2000 decade. With the expansion of these games, a whole bunch of tools have been developed to enhance the creation of indie games. For instance, Itch.io is a website specifically created to host tons of small indie games created by rather unknown people, like Daniel Linssen. These little pearls often rely on innovation, and originality, as we could compare to AAA games. In SuperHot, time moves only when you move, while in The Crypt of The Necrodancer, it’s everyone that moves only at the beat of the SoundTrack. Talking about music, AudioSurf creates a terrain based on your music, and in Dark Echo, you can only see through sound visualization. However, music isn’t the only part of a game, as developers are full of new ideas around gameplay, graphics, or storyline. Indie usually take a lot of time to be made as the budget isn’t very high. For instance, the recent Stardew Valley, have been developed by only one person, Eric Barone. Same thing goes for the fantastic Undertale, almost created only by Toby Fox, and in 3 years.</p>
+
+
+            <p id="main">JAMMING PICKAXES
+            </p>
+
+            <p>Though, not every game needs several years to be made from scratch. Some only take up to a few months, or weeks, or days, or even ... hours to be designed. Indeed, let me introduce you to Game Jams. Game Jams are contests, challenges created by developers, for developers, where people have a certain – rather short – amount of time to produce, generally from scratch, a game. They are often relying on a theme, that is decided just before the contest begins. From here, down to a few days (for the most-hardcore-jams) separate the theme revelation from the end of the submissions deadline. The most known jams are by far the Global Game Jam, and the Ludum Dare. This last contest is happening every 4 months from 2002. It has been created by Geoff Howland in April 2002, making it the first of its kind, and is now run by Mike Kasprzak. It started tiny, around a few friends and achieves now roughly 2000 entries for event. The Ludum Dare is composed of two version of itself, the Compo, and the Jam. The Compo is for the most experienced developers, where they have to build their games from the ground up, all alone, and furnish the source code afterwards, in 48 hours. The Jam is a cooler aspect of the Compo, where teams are allowed, as they have the right to use code samples and an extra day to submit their work. After this, the games are judged by the other creators themselves, and there are no prizes, except from having a finished game that others have enjoyed. Sometimes, games out from this contest gain success and encouragement and they finish being published online, with the other indie games. Though, while the Ludum Dare is the most known competition, a lot more of Jams are running every day, as they push game developers further into creating innovative and original little gems for us players to enjoy.</p>
+          </div>
+          <br>
+          <hr>
+
+          <div>
+            <div id="previous">
+              <button>previous article</button>
+              <figcaption> Aliquam erat volutpat</figcaption>
+            </div>
+
+            <div id="next">
+              <button>next article</button>
+              <figcaption> Aliquam erat volutpat</figcaption>
+            </div>
+          </div>
+
+          <p id="see_also">See Also:</p>
+
+          <?php include("../bottom.php"); ?>
+
+        <div class="black_bottom">
+          <p>LOUD MAGAZINE &copy; 2018<p>
+        </div>
+    </body>
+</html>
