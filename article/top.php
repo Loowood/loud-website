6bce5863b799edf36e47be52a8e5eff618c529d8
blublux <35227401+blublux@users.noreply.github.com>
2018-05-23 14:43:33 +0300

    Add files via upload

---
+++ article/top.php
@@ -0,0 +1,18 @@
+<?php
+echo "
+<div id=\"outerBox\">
+	<div id=\"upperBox\">
+		<a href=\"index.html\"><img id=\"placeholder-image\" src=\"../logo.png\"></a>
+		<div id=\"navbar\" class=\"headerDivs\">
+			<ul>
+				<a id=\"firstMenu\" href=\"culture.html\"><li> CULTURE </li></a>
+				<a href=\"entertainment.html\"><li> ENTERTAINMENT </li></a>
+				<a href=\"passions.html\"><li> PASSIONS </li></a>
+				<a href=\"student-life.html\"><li> STUDENT LIFE </li></a>
+				<a id=\"lastMenu\" href=\"partners.html\"><li> PARTNERS </li></a>
+			</ul>
+		</div>
+	</div>
+</div>
+";
+?>
