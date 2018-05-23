d48e5c7438b32167f5d7b5b7efc220b9039d9f3a
blublux <35227401+blublux@users.noreply.github.com>
2018-05-23 09:21:22 +0300

    Add files via upload

---
+++ article/4k_machine/4k_machine.php
@@ -0,0 +1,157 @@
+<html>
+    <head>
+        <meta charset=utf-8>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> LOUD 2018 </title>
+        <link style="text/css" rel="stylesheet" href="article.css">
+        <link style="text/css" rel="stylesheet" href="../commun.css">
+        <link href="article_template/oswald" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville|Oswald:300|Source+Sans+Pro:200,300,400" rel="stylesheet">
+    </head>
+    <body>
+        <?php include("../top.php"); ?>
+
+        <div class="white_square">
+          <div class="text">
+            <p id="header">4K EDITING MACHINE BUILD UNDER 2K
+</p>
+
+            <p id="first">First time builder reports his experience on building his first computer and all the knowledge he gained.
+
+            </p>
+
+            <p id="author">Text: Topi Puolakangas, Photos: Sami Huhtakangas
+
+            </p>
+
+            <img id="image1" src="editing1.png" alt="ponton">
+            <figcaption></figcaption>
+
+            <p>For some years I had been editing my videos on my old laptop but now it had became too slow for doing any work. I need to buy a new computer for serious editing work. I have been interested on custom computer builds for a while now. I have been following few channels on youtube who built, discuss and preview computers and their components for six months now. If I don’t have a computer of my own, I’m a carpenter without tools. I looked up my savings and decided my budget on 2000€ (2K). It was time for some research.
+
+            </p>
+
+            
+<h2>Research</h2>
+            <p>Since I was building my computer primarily for editing, I needed to build around my editing software of choice, Adobe Premiere and After Effects. They both require minimum of 8 Gb of RAM-memory to work (16 Gb is recommended).  I got one 16 Gb RAM-tick. I also found out that Graphics Card needs at least 8 Gb of memory to process 4K-video. When editing 4K-video you can lower your program monitors quality or use proxies to ease your workflow but you need that 8 Gb to render your video.
+One of the employees of my  local electronic retail store helped me for choosing my processor. He told me that most important aspect of processors are cores. Processors speeds are determined on their frequencys but if processor has multiple cores it can make multiple calculations same time. Intel processors tend to have faster cores, but AMD processors tend to have more cores on their processors. 
+            </p>
+
+              <figure>
+                <img id="image2" src="editing2.png" alt="poisson">
+              </figure>
+
+            <p>That’s why I chose AMD:s Ryzen 5 1600x as my processor. Motherboards has more differences than just size and witch processor they support. They also have different ports.
+One important thing to figure out is your coolers size because fan cooler can take a lot of space. You must have a big enough case to fit your cooler in. Also if you’re using a water cooler you must know if your case supports coolers radiator. There are different sizes of radiators you can instal, from 120 mm to 380 mm.
+I also considered using only hard drives for my file and software strorage since they are cheap. But a friend recomended that I should buy small (256 Gb) SSD where I instal my Operating System and important programs. Because SSD:s are much faster than hard drives I can open my programs much faster than from a hard drive. 
+
+            </p>
+<p id="main"> Processors speeds are determined on their frequencys but if processor has multiple cores it can make multiple calculations same time.
+            </p>
+            <h2>Mandatory components</h2>
+
+            <p>Motherboard is a giant circuitboard where you instal all the other components. Motherboards have different amount of slots for multiple components and can support limited amount of certain components (read your motherboards package and manual for best info). Motherboards also have different sizes. Most common sizes are ATX, micro-ATX and mini-ATX. They also have ports for your monitors, mics, speakers and your USB-devices and they are protected by  I/O shield. CPU, (Central Processing Unit) also known as
+Processor is the brain of your computer. Intel and AMD are the most common processor manufacturers. When choosing your processor keep in mind, that there are different motherboards for Intel and AMD processors. Your processor need a cooler to keep it working. There are two types of coolers; cheap fan coolers that may take room from your RAM slots and more expensive water coolers, that deeds a radiator installed on your case fans to work. 
+RAM-sticks stores data and machine code that’s currently being used. Many softwares require certain amount of RAM in order to work properly.
+GPU (Graphics Processing Unit) also knowns as Graphics Card is the most expensive component of your built. It accelerates creation of images on your computer. Modern games and complicated editing processes need lot of power on Graphics Card in order to work properly. Power Supply unit is a box that gives power to your computer. Case is where you instal your components and hard drive and/or SSD(Solid State Drive) is where you can store your programs and files. Difference in traditional hard drive and SSD is their writing and reading speed. SSD performs much faster than hard drive but is also more expensive, slower hard drives are cheap as dirt.
+
+            </p>
+			  <h2>Buying components</h2>
+<p>I bought most of my components from my local electronic retail stores. Only problem I had on buying components was with the graphics card. Ever since Bitcoin and other cryptocurrencys became popular, people started buying graphics cards to mine coins. It was really hard to get a good graphics card in whole of Finland. Importers had to delay lot of shipments in many stores because of their demande. This also raised their prices a lot. Hopefully when you read this prices have fallen and demand for crypto mining has fallen. In the end I managed to buy all of my components under the budget.
+	 <img id="image1" src="editing3.png" alt="ponton">
+            <figcaption></figcaption>
+
+            </p>
+			  <h2>IMPORTANT:
+Before you touch the motherboard, you must wear an antistatic wristband and attach the other end to something grounded. Keep your motherboard also on something grounded, you can put it on your motherboards box. Static electricity from your body might damage your motherboard. Read all of your manuals in order to instal your parts correctly! Especially the processor.
+</h2>
+			  <h2>Building</h2>
+			  <p>Firstly I installed my RAM-stick, processor and SSD. Before I moved my motherboard in the case, I attached all of my support-screws inside. Installing the motherboard was bit tricky since you must attach it to the support-screws and I/O shield at the same time but once I got it screwed still I could safely continue. Before I can attach any other big components I had to instal the PSU on the case and attach all the wires I had on the case fans and the motherboard because once the bigger components are installed, connections are really hard to get access to. Read your manuals before attaching any wires. When I was ready to instal the cooler I faced an unexpected problem; my radiator was 0,5 cm too big to attach it on my front fans. I could have easily removed my fans and move them but my lower fan wasn’t screwed like the other two. It was bolted in the case and the bolts prevented me from installing my radiator. I had to get a smaller radiator.
+When I got my new cooler and installed it on my back fan, I could continue my built. Graphics card and the router were easy to instal, like legos. After that I was DONE. And I booted my machine. My machine booted, but the screen did not find a singall. Not when using HDMI-  or VGA-cable. It turns out, that I needed a DVI-cable to connect my screen to my PC. It worked. My PC is running fine now. It had few issues with rebooting but I got it fixed when I tweaked some of my connection-pins. 
+In the end, now I have a great machine for editing witch I can upgrade piece by piece every two to three years when I need to. This was a really rewarding project that I was planning to do some time and I’m glad that I did it. In this project I’ve learned more about computers that I’ve thought I needed to know and I will continue learning when I’m going to upgrade my PC next time.
+
+            </p>
+			  <h2>My components and prices</h2>
+			  <table class="articleEditingTable">
+          <tr class="articleEditing">
+            <td class="articleEditing">Motherboard</td>
+            <td>Asus Prime B350-Plus AM4</td>
+            <td>99,00€</td>
+          </tr>
+          <tr>
+            <td>RAM</td>
+            <td>Adata XPG DD124 3000 MHZ 16 Gt</td>
+            <td>179,90€</td>
+          </tr>
+          <tr>
+            <td>Processor</td>
+            <td>AMD Ryzen 5 1600x </td>
+            <td>218,90€</td>
+          </tr>  
+          <tr>
+            <td>Processor Cooler</td>
+            <td>Cooler Master Masterliquid 120</td>
+            <td>89,90€</td>
+          </tr>  
+          <tr>
+            <td>Solid State Drive</td>
+            <td>Intel PRO 600P SSD 256Gt M.2</td>
+            <td>157,90€</td>
+          </tr>  
+          <tr>
+            <td>Hard Drive</td>
+            <td>Seagate Barracuta 3Tt</td>
+            <td>97,90€</td>
+          </tr>  
+          <tr>
+            <td>Graphics Card</td>
+            <td>Gigabyte G1 GTX 1080 8Gt</td>
+            <td>749,00€</td>
+          </tr>  
+          <tr>
+            <td>WiFi router</td>
+            <td>TP-Link Archer T9E</td>
+            <td>64,90€</td>
+          </tr>  
+          <tr>
+            <td>Case</td>
+            <td>Cooler Master Masterbox Lite 5 RGB</td>
+            <td>59,90€</td>
+          </tr>  
+          <tr>
+            <td class="articleEditing">Power Supply Unit</td>
+            <td class="articleEditing">Cooler Master Masterwatt Lite 700W</td>
+            <td>67,95€</td>
+          </tr>  
+          <tr>
+            <td>Total</td>
+            <td></td>
+            <td>1784,35€</td>
+          </tr>   
+        </table>
+          </div>
+
+          <br>
+          <hr>
+
+          <div>
+            <div id="previous">
+              <button>previous article</button>
+              <figcaption> Aliquam erat volutpat</figcaption>
+            </div>
+
+            <div id="next">
+              <button>next article</button>
+              <figcaption> Aliquam erat volutpat</figcaption>
+            </div>
+          </div>
+
+          <p id="see_also">See Also:</p>
+
+          <?php include("../bottom.php"); ?>
+
+        <div class="black_bottom">
+          <p>LOUD MAGAZINE &copy; 2018<p>
+        </div>
+    </body>
+</html>
---
+++ article/comics/comics.php
@@ -0,0 +1,85 @@
+<html>
+    <head>
+        <meta charset=utf-8>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> LOUD 2018 </title>
+        <link style="text/css" rel="stylesheet" href="article.css">
+        <link style="text/css" rel="stylesheet" href="../commun.css">
+        <link href="oswald" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville|Oswald:300|Source+Sans+Pro:200,300,400" rel="stylesheet">
+    </head>
+    <body>
+        <?php include("../top.php"); ?>
+
+        <div class="white_square">
+          <div class="text">
+            <p id="header">COMICS IN FINLAND</p>
+
+            <p id="first">Finland, country of lakes and saunas, also has an impressive number of diverse comics.
+            </p>
+
+            <p id="author">Text & Photography: Cambron Nicolas Pictures: Moomin Characters
+            </p>
+
+            <img id="image1" src="Moomin.jpg" alt="Moomin">
+            <figcaption>Moomin</figcaption>
+
+            <p>
+            This small nation in terms of population per kilometres square, has a curiously impressive amount of different genre of comic strips.
+            Nowadays artists try to create new style comic pages by picking templates from traditional art and entertainment.
+            The comics world has grown up, thanks to medias, as magazine advertisement or cartoons broadcast on TV.
+            It is henceforth inconceivable that Finnish magazines that didn’t have comics pages.
+            As you can imagine, comic strips have evolved after all these years as the evolution of thoughts.
+
+            The story begins in 1911, for the first Finnish comic book, Professori Itikaisen tutkimusretki (” Professor Mosquito’s Exploration”) is about a guy which explores the Eastern Asia. It was created by Ilamari Vaino and published by WSOY which refer to a Finnish publishing house owned by Bonnier the 21st of November. But the real expansion of interest about comics starts a bit later, during the end of 1920’s. Thanks to the support of new medias, most of comics strips were originate from Finland, and the first daily Finnish newspaper comic strip book was Herra Pulliainen (1927-33) by Akseli Halonen. It’s a traditional madcap humour, which describes a guy, seeing the world in hiws way.
+            </p>
+
+            <p id="main">MOOMIN AND TOM OF FINLAND EMERGED
+            </p>
+
+            <p>A couple of years after, one of the most important comic appeared, the Moomin, written by Tove Jansson in 1945. Later, she let her brother Lars Jansson take control. It was written, drawn and published until 1975.Moomin became very popular in a few years. It became also a real phenomenon all around the world and was published in 40 countries and in 120 newspapers. A couple of years after, one of the most important comic appeared, the Moomin, written by Tove Jansson in 1945. Later, she let her brother Lars Jansson take control. It was written, drawn and published until 1975.Moomin became very popular in a few years. It became also a real phenomenon all around the world and was published in 40 countries and in 120 newspapers.
+            </p>
+
+              <figure>
+                <img id="image2" src="pekka.jpg" alt="Pekka">
+                <figcaption id="figcaptionside">Pekka puupää</figcaption>
+              </figure>
+
+            <p>Another renowned international comic artist was “Tom of Finland” which is, more precisely in the internationawl gay community. His illustrations and comic had a real impact on the iconographic and interpretations of gay fantasies. That it took an important part on the gay community in the beginning of the 90’s.
+During the 1960s, Finnish comics take another way, sort of “comics revolution” appeared. Finnish comics began to break free at their reputation as children’s entertainment. To put us back in the context, pop art reaches its apex, and this brought everyday imagery into art. Timo Aarniala is one of the most popular artist that had mixed all different genre that could exist in his comics: topical political, commentary, slapstick humor and poetic stream of consciousness. In the same way, famous Finnish comic like  created by Ola Fogelberg, Herra Kerhonen, Veli Giovani (scriptwriter), Gosta Thilén, Veli Giovanni and Alexander Tawtiz.
+
+
+            <h2>FINNISH COMICS SOCIETY</h2>
+
+            <p>
+
+            The organisation has been working for Finnish comics since 1971. People started writing about comics for a magazine called Sarjainfo, which still exists after 40 years. Finnish Comics Society created an annual life-achievement award for comic artists, called Puupäähattu.
+            The Puupää Hat (Finnish: Puupäähattu) is the comic book prize awarded by the Suomen sarjakuvaseura every year since 1972. It rewards a Finnish comic book author for all of his work. His name is a tribute to the classic cartoon series Pekka Puupää (1925-1976). One of their main project was in 1970’s, with the creation of comic festival on Helsinki.
+            One of the most important steps in the evolution of Finnish comics has been the birth of the Finnish Comics Centre in 2008 Sarjakuvakeskus, is an open cultural centre for comics and related to art. This has given a new fresh breath onto the comic </p>
+            </p>
+
+          </div>
+          <br>
+          <hr>
+
+          <div>
+            <div id="previous">
+              <button>previous article</button>
+              <figcaption> Aliquam erat volutpat</figcaption>
+            </div>
+
+            <div id="next">
+              <button>next article</button>
+              <figcaption> Aliquam erat volutpat</figcaption>
+            </div>
+          </div>
+
+          <p id="see_also">See Also:</p>
+
+          <?php include("../bottom.php"); ?>
+
+        <div class="black_bottom">
+          <p>LOUD MAGAZINE &copy; 2018<p>
+        </div>
+    </body>
+</html>
