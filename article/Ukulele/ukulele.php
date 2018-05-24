4bbcb22a726adb21f713357137deb24f0462532d
NC <nchauveau94@gmail.com>
2018-05-24 08:29:05 +0300

    Add files via upload

---
+++ article/Ukulele/ukulele.php
@@ -0,0 +1,98 @@
+ <html>
+    <head>
+        <meta charset=utf-8>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> LOUD 2018 </title>
+        <link style="text/css" rel="stylesheet" href="article.css">
+        <link href="oswald" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville|Oswald:300|Source+Sans+Pro:200,300,400" rel="stylesheet">
+    </head>
+    <body>
+
+        <?php include("../top.php"); ?>
+
+        <div class="white_square">
+          <div class="text">
+            <p id="header">THE MAGIC SOUND OF UKULELE</p>
+
+            <p id="author">Text: Nathan Chauveau
+            </p>
+
+            <img id="image_uke2" src="uke_img.png" alt="ukulele">
+
+            <p>The ukulele is a music instrument that is associated with happiness and the beach. The instrument with this particular, smooth and magical sound is becoming more and more popular and many famous musicians have discovered it too and perform their music with this instrument. It has several advantages in the sense that it is quite simple to learn and does not cost much (80€ for a good quality ukulele). I will first explain how this little instrument was born and then give you some advices to get started.
+            </p>
+
+			<h2>The instrument from the islands</h2>
+
+            <p>The arrival of the ukulele is perfectly datable: everything began in 1879, a Portuguese ship &quot;Ravenscrag&quot; with its 419 immigrants
+coming from Madeira Islands to cultivate and pick sugar cane and pineapple, landed in Hawaii!</p>
+
+            <p>On board the ship were the three men held responsible for the introduction and transformation of a certain tradition of Portuguese lutherie: Manuel Nunes, José do Espírito Santo and Augusto Dias, three cabinet makers from Madeira who had knowledge of lutherie and apparently carried their small four-string Portuguese guitar. These guitars were called cavaquinho.
+			</p>
+
+            <p>Polynesians were fascinated by the sound of this little instrument, and word of mouth was such that it reached King David Kalakaua, the last king of Hawaii. This monarch was an aesthete, great dancer and musician, he was much more passionate about the arts and celebrations than for politics.
+			</p>
+
+            <p>The first ukuleles were therefore made with his blessing. Inspired by this cavaquinho, the ukulele is even smaller than its &quot;father&quot;
+and they were tuned differently.
+			</p>
+
+            <p>Although this musical instrument is relatively recent, the etymology of its name is uncertain. Among the most probable hypotheses, the term ukulele is thought to be a ukelele deformation. This name would have been formed by the two words &quot;uke&quot; and &quot;lele&quot;, which mean respectively &quot;to scratch&quot; and &quot;to strike&quot; which describe the movements of the hand which scratches the strings.</p>
+
+			<h2>Ready to start ?</h2>
+
+			<p>Learning ukulele is done with chord diagrams and a tablature (graphic score). I don&#39;t use scores that are too complicated to read (even for those who have already play a music instrument like me before) and very demotivating, most players do the same.</p>
+
+            <p>Even if you have never played an instrument, learning the ukulele is very easy. You must have a good method. Of course, you will have to play regularly, about 20 minutes a day to see progress quickly, that&#39;s the time I spent playing each day at the beginning.
+			</p>
+
+            <p>First you have to learn the basics of the ukulele. First I advise you to know some ukulele chords before you even want to play music piece. You will find the same basic notes in many music tracks that you will learn later. These notes are: C-Major, A-Minor,
+F-Major, G-Major, E-Minor, D-major. Play them in chains one after the other.
+			</p>
+
+            <p>You can do the same for some simple rhythms, called strumming patterns, which are more or less difficult to master. You have to practice brushing the strings up or down, in rhythm to get the desired effect. First thing, learn to count the 4 beats in rhythm. Then, start with the easiest rhythm: hit your strings only down, trying to keep the same rhythm. Here are some simple strumming patterns
+to get started:
+			</p>
+
+      <p>Shuffle: (Down Up | Down Up | Down Up | Down Up), the hand always makes the same movement, from bottom to top.</p>
+			<p>Church: (Down | Down Up | Down | Down Up)</p>
+			<p>Calypso: (Down | Down Up | Up | Down Up), which can be quickly mastered and present in many pieces.
+			</p>
+
+			<p>After playing and replaying these chords and strumming patterns in a loop to master them well, you can start by learning the songs of your choice (not too difficult at first). What I like about this instrument, is that you can learn to play almost any song in the world with a ukulele, you will easily find the tablature of your favourite song you want to learn on the Internet.
+			</p>
+
+            <img id="image_uke" src="uke_img2.png" alt="ukulele2" >
+
+			      <p id="main">What I like about this instrument, is that you can learn to play almost any song in the world with a ukulele.</p>
+
+            <h2>Inspiration and motivation</h2>
+
+            <p>There are many websites as well as YouTube channels which explain very well how to play such or such tracks and which also gives you some advice and habit to have. Here are some of them I started on: Ukulele teacher (YouTube channel), Ukulele cheats (YouTube channel), Michael Lynch (YouTube channel), ukuleletricks.com and ukulelehunt.com.</p>
+
+            <p>As time goes by, with experience you will progress quickly but there will always be a given moment when you will have the impression of stagnating. For me it was when you had to make crossed chords, no panic, it's normal, you just have to be patient and keep your motivation. Watch videos of the music you dream of learning in the future, it will motivate you more than anything.
+			       </p>
+
+          </div>
+
+          <div>
+            <div id="previous">
+              <button>previous article</button>
+              <figcaption> Aliquam erat volutpat</figcaption>
+            </div>
+
+            <div id="next">
+              <button>next article</button>
+              <figcaption> Aliquam erat volutpat</figcaption>
+            </div>
+          </div>
+
+          <p id="see_also">See Also:</p>
+
+          <?php include("../bottom.php"); ?>
+
+        <div class="black_bottom">
+          <p>LOUD MAGAZINE &copy; 2018<p>
+        </div>
+    </body>
